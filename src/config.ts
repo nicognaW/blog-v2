f42cd3d03415688a30920a7bcd77edabfe24d431
nicognaw <nicognaw@outlook.com>
2023-03-22 16:22:14 +0800

    Localizations & cursor & logo

--- src/config.ts
+++ src/config.ts
@@ -3,18 +3,18 @@ import type { Site, SocialObjects } from "@types";
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/",
   author: "NICO",
-  desc: "NK's website",
-  title: "NK's website",
-  ogImage: "astropaper-og.jpg",
+  desc: "NK's Insights",
+  title: "NK's Insights",
+  ogImage: "logo.jpeg",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
 
 export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 864,
-  height: 92,
+  enable: true,
+  svg: false,
+  width: 96,
+  height: 96,
 };
 
 export const SOCIALS: SocialObjects = [
@@ -22,6 +22,18 @@ export const SOCIALS: SocialObjects = [
     name: "Github",
     href: "https://github.com/nicognaw",
     linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:nicognaw@outlook.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "BiliBili",
+    href: "mailto:nicognaw@outlook.com",
+    linkTitle: `Send an email to ${SITE.title}`,
     active: true,
   },
 ];
